c90df8590c7d602ab8652a742e4ba46c22badd3a
Artem Shaban <ArtemShaban@gmail.com>
2020-06-18 00:18:56 +0300

    Add ktor netty server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val ktor_version = "1.3.2"
+
 plugins {
+    application
     kotlin("jvm") version "1.3.61"
 }
 
 repositories {
     jcenter()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<KotlinCompile>().all {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "io.ktor.server.netty.EngineMain"
 }
 
 
@@ -23,12 +40,15 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:0.24.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.24.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.24.1")
-    implementation ("mysql:mysql-connector-java:8.0.19")
-    implementation ("com.zaxxer:HikariCP:3.4.2")
+    implementation("mysql:mysql-connector-java:8.0.19")
+    implementation("com.zaxxer:HikariCP:3.4.2")
 
     //logging
-    implementation ("io.github.microutils:kotlin-logging:1.7.9")
-    implementation ("org.apache.logging.log4j:log4j-slf4j-impl:2.9.1")
+    implementation("io.github.microutils:kotlin-logging:1.7.9")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.9.1")
+
+    //ktor server
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
 
     //tests
     testImplementation("junit:junit:4.12")
--- src/main/kotlin/com/shaban/onliner/OnlinerTrends.kt
+++ src/main/kotlin/com/shaban/onliner/OnlinerTrends.kt
@@ -5,6 +5,15 @@ import com.shaban.onliner.dao.MySqlApartmentsDao
 import com.shaban.onliner.logic.Trend
 import com.shaban.onliner.logic.TrendsLogic
 import com.shaban.onliner.service.ApartmentsDataService
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
 import mu.KotlinLogging
 import java.time.Duration
 import java.time.Instant
@@ -18,20 +27,27 @@ import java.time.Instant
 + 1.a - trend of cost of flats (compare cost of every flat) Show like that: 1-room flat - 23% goes down, 77% goes up.  2-rooms flat ...
 1.b - trend of average cost square meter in 1-2-3-4 rooms flat in whole Minsk
 
-2 - start service at some cloud (amazon, google, azure)
+3 - start as service (ktot https://www.youtube.com/watch?v=zHQ7oBYSHrY)
+4 - start service at some cloud (amazon, google, azure)
 
-3 - visualize trend
-4 - make possible to see trends for area ( leonida bedy street)
+5 - visualize trend
+6 - make possible to see trends for area ( leonida bedy street)
 
 
  */
 
 val logger = KotlinLogging.logger {}
 
-fun main(args: Array<String>) {
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("My Example Blog2", ContentType.Text.Html)
+        }
+    }
 
     logger.info { "Hello, World!" }
-
 
     val apartmentsDao = MySqlApartmentsDao()
     apartmentsDao.initDatabase()
@@ -47,7 +63,7 @@ fun main(args: Array<String>) {
 
 private fun printAllFlatsTrend(apartmentsDao: MySqlApartmentsDao) {
     val trendOverAllFlatsList = TrendsLogic(apartmentsDao)
-            .getTrendOverAllFlatsSRx(Instant.now().minus(Duration.ofDays(4)), Instant.now())
+            .getTrendOverAllFlatsSRx(Instant.now().minus(Duration.ofDays(10)), Instant.now())
             .blockingGet()
 
     val map = trendOverAllFlatsList.map { it.first to Pair(it.second, it.third) }.toMap()
@@ -60,7 +76,7 @@ private fun printAllFlatsTrend(apartmentsDao: MySqlApartmentsDao) {
 private fun runApartmentsService(apartmentsDao: MySqlApartmentsDao) {
     ApartmentsDataService(ApartmentsLoader(), apartmentsDao)
             .runCRx()
-            .blockingAwait()
+            .subscribe() //todo
 }
 
 
