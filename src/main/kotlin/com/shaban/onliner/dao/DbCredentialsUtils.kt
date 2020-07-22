cf855785e62bfc1a9c59a734510afa25519d3141
Artem Shaban <ArtemShaban@gmail.com>
2020-07-22 23:11:11 +0300

    [Server] Add amazon db credentials.

--- src/main/kotlin/com/shaban/onliner/dao/DbCredentialsUtils.kt
+++ src/main/kotlin/com/shaban/onliner/dao/DbCredentialsUtils.kt
@@ -1,11 +1,33 @@
 package com.shaban.onliner.dao
 
-import com.shaban.onliner.dao.heroku.getHerokuJawsDbMariaCredentials
-
 fun getDbCredentials(): DbCredentials {
-    return getHerokuJawsDbMariaCredentials() ?: getLocalhostCredentials()
+    return getAmazonRdsMariaDbCredentials()
+            ?: getHerokuJawsDbMariaCredentials()
+            ?: getLocalhostCredentials()
+}
+
+fun getHerokuJawsDbMariaCredentials(): DbCredentials? {
+    val credString = System.getenv("JAWSDB_MARIA_URL") ?: return null
+    return parseDbCredentialString(credString)
+}
+
+fun getAmazonRdsMariaDbCredentials(): DbCredentials? {
+    val credString = System.getenv("AMAZON_MARIA_DB_URL") ?: return null
+    return parseDbCredentialString(credString)
 }
 
 fun getLocalhostCredentials(): DbCredentials {
     return DbCredentials("mysql://localhost", "root", "mysqlpassword", "r_onliner_trends")
+}
+
+/**
+credString - credential line with next format:  mysql://<username>:<password>@<host>/<dbname>
+ */
+private fun parseDbCredentialString(credString: String): DbCredentials {
+
+    val regex = Regex("mysql://(.+):(.+)@(.+)/(.+)")
+    val matchResult = regex.matchEntire(credString)
+            ?: throw  IllegalArgumentException("Can not parse credential string for jawsdb maria from environment variable 'JAWSDB_MARIA_URL'")
+    val groupValues = matchResult.groupValues
+    return DbCredentials("mysql://" + groupValues[3], groupValues[1], groupValues[2], groupValues[4])
 }
